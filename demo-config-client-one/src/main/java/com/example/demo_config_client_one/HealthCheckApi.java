feb335155b51e3f71166a3fc36ebae2a420775e0
hgene2452 <hgene0929@gmail.com>
2024-07-22 15:46:07 +0900

    :sparkles: [config-files] upload config files

--- demo-config-client-one/src/main/java/com/example/demo_config_client_one/HealthCheckApi.java
+++ demo-config-client-one/src/main/java/com/example/demo_config_client_one/HealthCheckApi.java
@@ -1,10 +1,12 @@
 package com.example.demo_config_client_one;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequiredArgsConstructor
 public class HealthCheckApi {
