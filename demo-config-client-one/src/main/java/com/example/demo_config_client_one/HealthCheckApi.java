52890cd57cbe0f06ed112f7c789b29c8f922ed55
hgene2452 <hgene0929@gmail.com>
2024-07-22 15:41:24 +0900

    :sparkles: [config-files] upload config files

--- demo-config-client-one/src/main/java/com/example/demo_config_client_one/HealthCheckApi.java
+++ demo-config-client-one/src/main/java/com/example/demo_config_client_one/HealthCheckApi.java
@@ -1,22 +1,18 @@
 package com.example.demo_config_client_one;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class IndexController {
+public class HealthCheckApi {
 
-    private final Config config;
+    private final Environment env;
 
     @GetMapping("/")
-    public String index() {
-        return "Hello World";
-    }
-
-    @GetMapping("/config")
-    public String config() {
-        return config.toString();
+    public String healthCheck() {
+        return String.format("app is up and running\nstatus: %s", env.getProperty("status"));
     }
 }
