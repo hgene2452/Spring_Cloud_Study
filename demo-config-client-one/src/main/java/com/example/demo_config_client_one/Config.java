4434ef2c384a31b59e35579c3cc60e5ef90b159d
hgene2452 <hgene0929@gmail.com>
2024-07-22 14:57:24 +0900

    :sparkles: [config-files] upload config files

---
+++ demo-config-client-one/src/main/java/com/example/demo_config_client_one/Config.java
@@ -0,0 +1,9 @@
+package com.example.demo_config_client_one;
+
+import lombok.Getter;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@Getter
+@RefreshScope
+public class Config {
+}
