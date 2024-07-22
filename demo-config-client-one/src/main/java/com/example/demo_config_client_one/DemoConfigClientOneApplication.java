ebce64f4d866854ce650a17c494e3f17a6ec26e5
hgene2452 <hgene0929@gmail.com>
2024-07-22 13:32:10 +0900

    :sparkles: [config-client] init & settings

---
+++ demo-config-client-one/src/main/java/com/example/demo_config_client_one/DemoConfigClientOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_config_client_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConfigClientOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConfigClientOneApplication.class, args);
+	}
+
+}
