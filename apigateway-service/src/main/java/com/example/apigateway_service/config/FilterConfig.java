9c8aab2e2292e55c7ae9a12d9b36ab4108e6d31e
hgene2452 <hgene0929@gmail.com>
2024-08-02 04:58:38 +0900

    :sparkles: [api-gateway] application.yml을 통한 G/W 설정

--- apigateway-service/src/main/java/com/example/apigateway_service/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/example/apigateway_service/config/FilterConfig.java
@@ -5,10 +5,11 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// application.yml 을 통해 설정할 경우 주석처리
+// @Configuration
 public class FilterConfig {
 
-	@Bean
+	// @Bean
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 			.route(r -> r.path("/first-service/**")
